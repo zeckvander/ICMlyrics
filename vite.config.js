2a5a959bdf5458ecc8f152dad673f21c15b3dde8
zeckvander <zeckvander@gmail.com>
2026-07-11 10:44:19 -0300

    add sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path' 
+import Sitemap from 'vite-plugin-sitemap' // 1. Importe o plugin
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    Sitemap({ // 2. Adicione-o aqui
+      hostname: 'https://ic-mlyrics.vercel.app/',
+    })
   ],
   resolve: {
     alias: {
