b25f6f7b86048cc4003410c4168d3e39bd3de264
zeckvander <zeckvander@gmail.com>
2026-07-14 18:39:57 -0300

    atualizações

--- public/data/biblia/importar.cjs
+++
@@ -1,7 +0,0 @@
-// Exemplo: Buscar o capítulo 1 de Gênesis na versão ACF
-const { data, error } = await supabase
-  .from('versiculos')
-  .select('texto, versiculo')
-  .eq('book_abbrev', 'gn')
-  .eq('capitulo', 1)
-  .eq('versao', 'acf');
