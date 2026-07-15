d4c2deace79a815a4e03be0d1440c963a82fc3c0
zeckvander <zeckvander@gmail.com>
2026-07-15 13:12:41 -0300

    fix: desativar geracao automatica de robots

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path' 
-import Sitemap from 'vite-plugin-sitemap' // 1. Importe o plugin
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    Sitemap({ // 2. Adicione-o aqui
+    Sitemap({
       hostname: 'https://ic-mlyrics.vercel.app/',
+      generateRobots: false, // ADICIONE ESTA LINHA
     })
   ],
   resolve: {
