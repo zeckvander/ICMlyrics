f528e19eb3a5304d94c3ac0002d7b42129e14e07
zeckvander <zeckvander@gmail.com>
2026-07-15 13:18:12 -0300

    removendo plugin sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path' 
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    Sitemap({
-      hostname: 'https://ic-mlyrics.vercel.app/',
-      generateRobots: false, // ADICIONE ESTA LINHA
-    })
+    // REMOVA O BLOCO DO Sitemap AQUI
   ],
   resolve: {
     alias: {
